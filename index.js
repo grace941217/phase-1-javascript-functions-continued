f4011edf85b8386777fa851b9bec4de07ed3909f
Grace <grace941217@gmail.com>
2022-09-21 18:31:04 -0400

    commit tree 579f2976cf07f7a2c635cc9a9e1d30046d2e19c6

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+    return (`This Saturday, I want to ${target}!`);
+}
+
+function mondayWork(place = "go to the office") {
+    return (`This Monday, I will ${place}.`);
+}
+
+// function wrapAdjective(flair ='*', adj){
+//     let wrappedSentence = function (adj = 'a hard worker'){
+//         return `You are ${flair}${adj}${flair}!`
+//     }
+//     return wrappedSentence;
+//     }
+
+// let wrapAdjective = function(style="*") {
+//     return function(adjective="special") {
+//       return `You are ${style}${adjective}${style}!`
+//     }
+//   }
+
+function wrapAdjective(flair = "*"){
+    return function (wrappedSentence = "a hard worker"){
+        return `You are ${flair}${wrappedSentence}${flair}!`;
+    };
+}
